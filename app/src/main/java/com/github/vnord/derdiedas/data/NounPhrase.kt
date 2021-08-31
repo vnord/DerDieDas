ca94cd8e9570e5c499385b62ffcf65f9c72fd194
Ari von Nordenskjöld <ari@vnord.net>
2021-08-31 22:40:03 +0200

    Add additional columns to NounPhrase for keeping track of review info

--- app/src/main/java/com/github/vnord/derdiedas/data/NounPhrase.kt
+++ app/src/main/java/com/github/vnord/derdiedas/data/NounPhrase.kt
@@ -9,8 +9,23 @@ data class NounPhrase(
     @PrimaryKey
     @ColumnInfo(name = "noun")
     val noun: String,
+
     @ColumnInfo(name = "gender")
-    val gender: Gender
+    val gender: Gender,
+
+    @ColumnInfo(name = "last_review")
+    val lastReview: Long? = null,
+
+    @ColumnInfo(name = "next_review")
+    val nextReview: Long? = null,
+
+    @ColumnInfo(name = "reviews_left")
+    val reviewsLeft: Int = NUMBER_OF_REVIEWS_REQUIRED,
+
+    @ColumnInfo(name = "reviews_done")
+    val reviewsDone: Int = 0
 )
 
 enum class Gender(val str: String) { DER("der"), DIE("die"), DAS("das") }
+
+const val NUMBER_OF_REVIEWS_REQUIRED = 5
--- app/src/main/java/com/github/vnord/derdiedas/data/NounPhraseRoomDatabase.kt
+++ app/src/main/java/com/github/vnord/derdiedas/data/NounPhraseRoomDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [NounPhrase::class], version = 1, exportSchema = false)
+@Database(entities = [NounPhrase::class], version = 3, exportSchema = false)
 abstract class NounPhraseRoomDatabase : RoomDatabase() {
     abstract fun nounPhraseDao(): NounPhraseDao
 
